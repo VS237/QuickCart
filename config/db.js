9714c829fa7af254188dbd1194577cfa128861a4
VS237 <victorinsonhofo@gmail.com>
2025-05-30 01:46:31 +0200

    first commit

--- config/db.js
+++ config/db.js
@@ -19,7 +19,7 @@ async function connectDB
       bufferCommands:false
     }
 
-    cached.promise = mongoose.connect('${process.env.MONGODB_URI}/quickcart',opts).then( mongoose => {return mongoose})
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then( mongoose => {return mongoose})
   }
 
   cached.conn = await cached.promise
